4e0df3bf1f508f234ce9b9290a543799a294dc78
daerkns <daerkns@gmail.com>
2025-03-20 22:16:35 +0600

    fixing blockchain startup issues due to already containing old data

---
+++ frontend/shared/contracts.js
@@ -0,0 +1,8 @@
+// Contract addresses from deployment on March 20, 2025
+const CONTRACT_ADDRESSES = {
+  EHRRegistry: '0x42699A7612A82f1d9C36148af9C77354759b210b',
+  AccessControl: '0xa50a51c09a5c451C52BB714527E1974b686D8e77',
+  PatientRecords: '0x9a3DBCa554e9f6b9257aAa24010DA8377C57c17e'
+};
+
+export default CONTRACT_ADDRESSES;
